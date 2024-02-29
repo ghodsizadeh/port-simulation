04c8015ad1f8d749cc11a48a5e7112d9de5abba0
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2024-02-29 09:22:24 +0100

    Add config file with simulation settings

---
+++ maersk_task/config.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+
+SIMULATION_TIME_IN_MINUTES = 60
+
+@dataclass(slots=True, frozen=True)
+class Config:
+    vessel_average_in_minutes: int = 5 * 60
+
+    berth_count: int = 2
+
+    crane_count: int = 2
+    crane_time_in_minutes: int = 3
+
+    truck_count: int = 3
+    truck_time_in_minutes: int = 6
+
+    simulation_time_in_minutes: int = SIMULATION_TIME_IN_MINUTES
+    
+
+
+
+config = Config()
+
+
+if __name__ == "__main__":
+    print(config)
